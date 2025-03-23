f7a3fc977a7a5ab1b2896c89bde5cf52b436ceee
Prajwal M <studies.prajwalm@gmail.com>
2025-03-23 18:18:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,293 +1,151 @@
-"""
-Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized Discord bot in Python
-
-Version: 6.2.0
-"""
-
-import json
-import logging
 import os
-import platform
-import random
-import sys
-
-import aiosqlite
 import discord
-from discord.ext import commands, tasks
-from discord.ext.commands import Context
+import random
+import asyncio
+from discord.ext import commands
 from dotenv import load_dotenv
 
-import DatabaseManager
-
-if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
-    sys.exit("'config.json' not found! Please add it and try again.")
-else:
-    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
-        config = json.load(file)
-
-"""	
-Setup bot intents (events restrictions)
-For more information about intents, please go to the following websites:
-https://discordpy.readthedocs.io/en/latest/intents.html
-https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
-
-
-Default Intents:
-intents.bans = True
-intents.dm_messages = True
-intents.dm_reactions = True
-intents.dm_typing = True
-intents.emojis = True
-intents.emojis_and_stickers = True
-intents.guild_messages = True
-intents.guild_reactions = True
-intents.guild_scheduled_events = True
-intents.guild_typing = True
-intents.guilds = True
-intents.integrations = True
-intents.invites = True
-intents.messages = True # `message_content` is required to get the content of the messages
-intents.reactions = True
-intents.typing = True
-intents.voice_states = True
-intents.webhooks = True
-
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
-intents.members = True
-intents.message_content = True
-intents.presences = True
-"""
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
+users = {}
 
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
-# intents.message_content = True
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 
-# Setup both of the loggers
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
 
+@bot.command()
+async def say(ctx, *, message):
+    await ctx.send(message)
 
-class LoggingFormatter(logging.Formatter):
-    # Colors
-    black = "\x1b[30m"
-    red = "\x1b[31m"
-    green = "\x1b[32m"
-    yellow = "\x1b[33m"
-    blue = "\x1b[34m"
-    gray = "\x1b[38m"
-    # Styles
-    reset = "\x1b[0m"
-    bold = "\x1b[1m"
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}! 👋")
 
-    COLORS = {
-        logging.DEBUG: gray + bold,
-        logging.INFO: blue + bold,
-        logging.WARNING: yellow + bold,
-        logging.ERROR: red,
-        logging.CRITICAL: red + bold,
-    }
+@bot.command()
+async def userinfo(ctx, member: discord.Member = None):
+    member = member or ctx.author
+    embed = discord.Embed(title=f"User Info - {member.name}", color=discord.Color.blue())
+    embed.add_field(name="Username", value=member.name, inline=True)
+    embed.add_field(name="ID", value=member.id, inline=True)
+    embed.set_thumbnail(url=member.avatar.url if member.avatar else "")
+    await ctx.send(embed=embed)
 
-    def format(self, record):
-        log_color = self.COLORS[record.levelno]
-        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
-        format = format.replace("(reset)", self.reset)
-        format = format.replace("(levelcolor)", log_color)
-        format = format.replace("(green)", self.green + self.bold)
-        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
-        return formatter.format(record)
+@bot.command()
+async def serverinfo(ctx):
+    embed = discord.Embed(title="Server Info", color=discord.Color.green())
+    embed.add_field(name="Server Name", value=ctx.guild.name, inline=True)
+    embed.add_field(name="Total Members", value=ctx.guild.member_count, inline=True)
+    await ctx.send(embed=embed)
 
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int = 5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f"Deleted {amount} messages.", delete_after=3)
 
-logger = logging.getLogger("discord_bot")
-logger.setLevel(logging.INFO)
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason="No reason provided"):
+    await member.kick(reason=reason)
+    await ctx.send(f"{member.mention} has been kicked.")
 
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(LoggingFormatter())
-# File handler
-file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-file_handler_formatter = logging.Formatter(
-    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
-)
-file_handler.setFormatter(file_handler_formatter)
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason="No reason provided"):
+    await member.ban(reason=reason)
+    await ctx.send(f"{member.mention} has been banned.")
 
-# Add the handlers
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
-
-
-class DiscordBot(commands.Bot):
-    def __init__(self) -> None:
-        super().__init__(
-            command_prefix=commands.when_mentioned_or(config["prefix"]),
-            intents=intents,
-            help_command=None,
-        )
-        """
-        This creates custom bot variables so that we can access these variables in cogs more easily.
-
-        For example, The config is available using the following code:
-        - self.config # In this class
-        - bot.config # In this file
-        - self.bot.config # In cogs
-        """
-        self.logger = logger
-        self.config = config
-        self.database = None
-
-    async def init_db(self) -> None:
-        async with aiosqlite.connect(
-            f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
-        ) as db:
-            with open(
-                f"{os.path.realpath(os.path.dirname(__file__))}/database/schema.sql"
-            ) as file:
-                await db.executescript(file.read())
-            await db.commit()
-
-    async def load_cogs(self) -> None:
-        """
-        The code in this function is executed whenever the bot will start.
-        """
-        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
-            if file.endswith(".py"):
-                extension = file[:-3]
-                try:
-                    await self.load_extension(f"cogs.{extension}")
-                    self.logger.info(f"Loaded extension '{extension}'")
-                except Exception as e:
-                    exception = f"{type(e).__name__}: {e}"
-                    self.logger.error(
-                        f"Failed to load extension {extension}\n{exception}"
-                    )
-
-    @tasks.loop(minutes=1.0)
-    async def status_task(self) -> None:
-        """
-        Setup the game status task of the bot.
-        """
-        statuses = ["with you!", "with Krypton!", "with humans!"]
-        await self.change_presence(activity=discord.Game(random.choice(statuses)))
-
-    @status_task.before_loop
-    async def before_status_task(self) -> None:
-        """
-        Before starting the status changing task, we make sure the bot is ready
-        """
-        await self.wait_until_ready()
-
-    async def setup_hook(self) -> None:
-        """
-        This will just be executed when the bot starts the first time.
-        """
-        self.logger.info(f"Logged in as {self.user.name}")
-        self.logger.info(f"discord.py API version: {discord.__version__}")
-        self.logger.info(f"Python version: {platform.python_version()}")
-        self.logger.info(
-            f"Running on: {platform.system()} {platform.release()} ({os.name})"
-        )
-        self.logger.info("-------------------")
-        await self.init_db()
-        await self.load_cogs()
-        self.status_task.start()
-        self.database = DatabaseManager(
-            connection=await aiosqlite.connect(
-                f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
-            )
-        )
-
-    async def on_message(self, message: discord.Message) -> None:
-        """
-        The code in this event is executed every time someone sends a message, with or without the prefix
-
-        :param message: The message that was sent.
-        """
-        if message.author == self.user or message.author.bot:
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, *, member_name):
+    banned_users = await ctx.guild.bans()
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if user.name == member_name:
+            await ctx.guild.unban(user)
+            await ctx.send(f"{user.mention} has been unbanned.")
             return
-        await self.process_commands(message)
+    await ctx.send("User not found in ban list.")
 
-    async def on_command_completion(self, context: Context) -> None:
-        """
-        The code in this event is executed every time a normal command has been *successfully* executed.
+@bot.command()
+async def mute(ctx, member: discord.Member, time: int = 60):
+    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    if not muted_role:
+        muted_role = await ctx.guild.create_role(name="Muted")
+        for channel in ctx.guild.channels:
+            await channel.set_permissions(muted_role, send_messages=False)
 
-        :param context: The context of the command that has been executed.
-        """
-        full_command_name = context.command.qualified_name
-        split = full_command_name.split(" ")
-        executed_command = str(split[0])
-        if context.guild is not None:
-            self.logger.info(
-                f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
-            )
-        else:
-            self.logger.info(
-                f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
-            )
+    await member.add_roles(muted_role)
+    await ctx.send(f"{member.mention} has been muted for {time} seconds.")
+    await asyncio.sleep(time)
+    await member.remove_roles(muted_role)
+    await ctx.send(f"{member.mention} is now unmuted.")
 
-    async def on_command_error(self, context: Context, error) -> None:
-        """
-        The code in this event is executed every time a normal valid command catches an error.
+@bot.command()
+async def unmute(ctx, member: discord.Member):
+    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    if muted_role in member.roles:
+        await member.remove_roles(muted_role)
+        await ctx.send(f"{member.mention} has been unmuted.")
 
-        :param context: The context of the normal command that failed executing.
-        :param error: The error that has been faced.
-        """
-        if isinstance(error, commands.CommandOnCooldown):
-            minutes, seconds = divmod(error.retry_after, 60)
-            hours, minutes = divmod(minutes, 60)
-            hours = hours % 24
-            embed = discord.Embed(
-                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
-                color=0xE02B2B,
-            )
-            await context.send(embed=embed)
-        elif isinstance(error, commands.NotOwner):
-            embed = discord.Embed(
-                description="You are not the owner of the bot!", color=0xE02B2B
-            )
-            await context.send(embed=embed)
-            if context.guild:
-                self.logger.warning(
-                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the guild {context.guild.name} (ID: {context.guild.id}), but the user is not an owner of the bot."
-                )
-            else:
-                self.logger.warning(
-                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's DMs, but the user is not an owner of the bot."
-                )
-        elif isinstance(error, commands.MissingPermissions):
-            embed = discord.Embed(
-                description="You are missing the permission(s) `"
-                + ", ".join(error.missing_permissions)
-                + "` to execute this command!",
-                color=0xE02B2B,
-            )
-            await context.send(embed=embed)
-        elif isinstance(error, commands.BotMissingPermissions):
-            embed = discord.Embed(
-                description="I am missing the permission(s) `"
-                + ", ".join(error.missing_permissions)
-                + "` to fully perform this command!",
-                color=0xE02B2B,
-            )
-            await context.send(embed=embed)
-        elif isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(
-                title="Error!",
-                # We need to capitalize because the command arguments have no capital letter in the code and they are the first word in the error message.
-                description=str(error).capitalize(),
-                color=0xE02B2B,
-            )
-            await context.send(embed=embed)
-        else:
-            raise error
+@bot.command()
+async def fun(ctx):
+    responses = ["You're awesome! 😃", "Keep going! 🚀", "You got this! 💪"]
+    await ctx.send(random.choice(responses))
 
+@bot.command()
+async def eightball(ctx, *, question):
+    responses = ["Yes!", "No!", "Maybe...", "Definitely!", "Not a chance!"]
+    await ctx.send(f"🎱 {random.choice(responses)}")
 
-load_dotenv()
+@bot.command()
+async def flip(ctx):
+    await ctx.send(random.choice(["Heads!", "Tails!"]))
 
-bot = DiscordBot()
-bot.run('OTg1MTkxNjY0ODQwNzU3MzIy.GZ3OXi.p_QpJM4RXC-viwQSgUM_egZn1CBIuCtsQOx4Go')
+@bot.command()
+async def balance(ctx):
+    user = str(ctx.author.id)
+    if user not in users:
+        users[user] = {"balance": 100}
+    await ctx.send(f"{ctx.author.mention}, your balance is 💰 {users[user]['balance']} coins.")
+
+@bot.command()
+async def work(ctx):
+    user = str(ctx.author.id)
+    if user not in users:
+        users[user] = {"balance": 100}
+    
+    earnings = random.randint(10, 50)
+    users[user]["balance"] += earnings
+    await ctx.send(f"{ctx.author.mention}, you worked and earned 💰 {earnings} coins! Your new balance is {users[user]['balance']}.")
+
+@bot.command()
+async def give(ctx, member: discord.Member, amount: int):
+    giver = str(ctx.author.id)
+    receiver = str(member.id)
+
+    if giver not in users:
+        users[giver] = {"balance": 100}
+    if receiver not in users:
+        users[receiver] = {"balance": 100}
+
+    if users[giver]["balance"] >= amount and amount > 0:
+        users[giver]["balance"] -= amount
+        users[receiver]["balance"] += amount
+        await ctx.send(f"{ctx.author.mention} gave 💰 {amount} coins to {member.mention}!")
+    else:
+        await ctx.send("You don't have enough coins.")
+
+if TOKEN:
+    bot.run(TOKEN)
+else:
+    print("Error: DISCORD_TOKEN not found in .env file")
